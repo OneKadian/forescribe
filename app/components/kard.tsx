4dcc09eb6c6504f9c01123dd28f8c3d53d700303
Anirudh Kadian <coderanirudh@gmail.com>
2025-01-16 13:19:16 +0530

    solved ts issue

--- app/components/kard.tsx
+++ app/components/kard.tsx
@@ -1,30 +1,4 @@
-// import Image from "next/image"
 
-
-// interface CardProps {
-//   title: string
-//   subtitle: string
-//   imageUrl: string
-//   bgColor: string
-// }
-
-// export function Card({ title, subtitle, imageUrl, bgColor }: CardProps) {
-//   return (
-//   <div className={`h-[33vh] rounded-2xl ${bgColor} p-3`}>
-//     <div className="h-[40%] w-full overflow-hidden rounded-lg">
-//       <img
-//         src={imageUrl}
-//         alt={`${title} logo`}
-//         className="h-full w-full object-cover"
-//       />
-//     </div>
-//     <div className="mt-3 space-y-1 text-center">
-//       <h3 className="text-xl font-semibold text-[#19154E]">{title}</h3>
-//       <p className="text-sm font-semibold text-black">{subtitle}</p>
-//     </div>
-//   </div>
-//   )
-// }
 
 import Image from "next/image";
 
--- app/components/kolumn.tsx
+++ app/components/kolumn.tsx
@@ -1,42 +1,4 @@
-// "use client"
 
-// import { motion } from "framer-motion"
-// import { Card } from "./kard"
-
-// interface ColumnProps {
-//   items: {
-//     name: string
-//     logo: string
-//     type: string
-//     bgColor: string
-//   }[]
-//   duration: number
-// }
-
-// const Column = ({ items, duration }) => {
-//   const doubledItems = [...items, ...items]; // Repeat items for scrolling effect
-
-//   return (
-//     <div className="relative h-full flex flex-col gap-2 overflow-hidden py-4">
-//       <motion.div
-//         initial={{ y: 0 }}
-//         animate={{ y: "-50%" }}
-//         transition={{
-//           duration,
-//           repeat: Infinity,
-//           ease: "linear",
-//         }}
-//         className="flex flex-col gap-2"
-//       >
-//         {doubledItems.map((item, index) => (
-//           <Card key={`${item.title}-${index}`} {...item} />
-//         ))}
-//       </motion.div>
-//     </div>
-//   );
-// };
-
-// export default Column
 
 "use client";
 
