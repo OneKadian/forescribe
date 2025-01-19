14b05cea06dac0f65d26d14acad5d98cfc0ef1cb
Anirudh Kadian <coderanirudh@gmail.com>
2025-01-19 23:13:03 +0530

    added slack2

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -86,7 +86,7 @@ const Contact: React.FC<ContactProps> = ({ firstName }) => {
                 name="email"
                 id="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="name@flowbite.com"
+                placeholder="name@forescribe.com"
                 required
               />
             </div>
